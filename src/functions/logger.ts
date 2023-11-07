63fc42d6c039318b6c23d6cf54cf0c5cf98e0ca6
Austin Best <austin.w.best@gmail.com>
2023-11-06 21:55:16 -0500

    Adjust winston logging order

--- src/functions/logger.ts
+++ src/functions/logger.ts
@@ -1,7 +1,18 @@
 import { createLogger, format, transports } from 'winston';
 import config from '../config.js';
 
+const logLevels = {
+    error: 0,
+    warn: 1,
+    info: 2,
+    verbose: 3,
+    http: 4,
+    debug: 5,
+    silly: 6
+};
+
 export default createLogger({
+    levels: logLevels,
     transports: [
         new transports.File({
             filename: 'error.log',
@@ -10,7 +21,7 @@ export default createLogger({
             maxFiles: 5,
             tailable: true,
             format: format.combine(format.timestamp(), format.json()),
-            level: 'error',
+            level: 'error'
         }),
         new transports.File({
             filename: 'combined.log',
